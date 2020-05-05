a448591350bffa4cf36ddba63c1d2391dacee642
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-05 09:40:19 +0200

    Unit tests added

--- LinearDataStructures/UnitTestProject1/Properties/AssemblyInfo.cs
+++ LinearDataStructures/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("LinearDataStructuresTests")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinearDataStructuresTests")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("40185e08-29db-4285-abf7-b98d3b8a0584")]
+[assembly: Guid("ce8c1fcb-c766-47db-a2ea-5fed40174882")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- LinearDataStructures/UnitTestProject1/UnitTest1.cs
+++ LinearDataStructures/UnitTestProject1/UnitTest1.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LinearDataStructures;
 
-namespace LinearDataStructuresTests
+namespace UnitTestProject1
 {
     [TestClass]
-    public class LinkedListsTests
+    public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
