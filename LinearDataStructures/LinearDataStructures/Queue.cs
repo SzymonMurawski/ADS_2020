c4becb3afa4f4224a2c0c6a1b651db78b8957ab5
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-05 10:56:18 +0200

    Using Count from inner List

--- LinearDataStructures/LinearDataStructures/Queue.cs
+++ LinearDataStructures/LinearDataStructures/Queue.cs
@@ -9,22 +9,20 @@ namespace LinearDataStructures
     public class Queue
     {
         private LinkedList List { get; set; }
-        public int Count { get; set; }
+        // public int Count { get; set; }
+        public int Count { get { return List.Count; } }
         public Queue()
         {
-            Count = 0;
             List = new LinkedList();
         }
         public void Enqueue(int data)
         {
             List.AddLast(data);
-            Count++;
         }
         public int Dequeue()
         {
             int data = List.First.Data;
             List.RemoveFirst();
-            Count--;
             return data;
         }
         public int Peek()
