a63be6d955c9beedfa027e4e8c692971093d7937
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-05 10:31:13 +0200

    Queue definition

---
+++ LinearDataStructures/LinearDataStructures/Queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructures
+{
+    public class Queue
+    {
+        private LinkedList List { get; set; }
+        public int Count { get; set; }
+        public Queue()
+        {
+            Count = 0;
+            List = new LinkedList();
+        }
+        public void Enqueue(int data)
+        {
+            throw new NotImplementedException();
+        }
+        public int Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+        public int Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
