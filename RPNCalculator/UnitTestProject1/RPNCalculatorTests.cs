ac9357b1af1603c3f3698a1bd3b47f087a5d8fa2
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-12 09:57:43 +0200

    Init

---
+++ RPNCalculator/UnitTestProject1/RPNCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPNCalculator;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RPNCalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
