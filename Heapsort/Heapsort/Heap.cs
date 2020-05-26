3aac642ea254702f86e190b826f7d2c3773e79e8
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-26 09:46:44 +0200

    Added heap properties

---
+++ Heapsort/Heapsort/Heap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heapsort
+{
+    public class Heap
+    {
+        public int[] Array { get; set; }
+        public int Size { get; set; }
+        public Heap(int[] array)
+        {
+            Array = array;
+            Size = array.Length;
+            //TODO: implement Floyd's algorithm for creating a heap from an array
+        }
+    }
+}
