a131c3e8e594bef2cf402541294fa3c6803cc7a8
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-26 09:51:15 +0200

    Added Unit Test project

---
+++ Heapsort/UnitTests/HeapTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Heapsort;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HeapTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
